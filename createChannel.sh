2c04effa2994688d34bab2e8dda1792a139faf01
aniket <aniket.r.gupta@slrtce.in>
2024-03-11 19:31:01 +0530

    added more 2 peers

--- createChannel.sh
+++ createChannel.sh
@@ -20,12 +20,27 @@ setGlobalsForPeer0Org1(){
     export CORE_PEER_ADDRESS=localhost:7051
 }
 
+setGlobalsForPeer1Org1(){
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7061
+    
+}
 
 setGlobalsForPeer0Org2(){
     export CORE_PEER_LOCALMSPID="Org2MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
     export CORE_PEER_ADDRESS=localhost:9051
+    
+}
+
+setGlobalsForPeer1Org2(){
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9061
     
 }
 
@@ -39,14 +54,19 @@ createChannel(){
     --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
 }
 
-
 joinChannel(){
     setGlobalsForPeer0Org1
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-
+    
+    setGlobalsForPeer1Org1
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
     setGlobalsForPeer0Org2
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-
+    
+    setGlobalsForPeer1Org2
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
 }
 
 updateAnchorPeers(){
@@ -55,7 +75,7 @@ updateAnchorPeers(){
     
     setGlobalsForPeer0Org2
     peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
-
+    
 }
 
 createChannel
